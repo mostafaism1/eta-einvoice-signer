90c752eb6c1b79b8e71ce3bb96b3a38e7ddae5f8
Mostafa Hassanein <me.cssign@gmail.com>
2022-09-20 07:16:57 +0200

    Generate a spring-boot project

---
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/EtaEinvoiceSignerApplication.java
@@ -0,0 +1,13 @@
+package com.github.mostafaism1.etaeinvoicesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaEinvoiceSignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtaEinvoiceSignerApplication.class, args);
+	}
+
+}
