f970143e09bd183ba22645c9ee06b02b4ef5b790
Mostafa Hassanein <me.cssign@gmail.com>
2022-09-23 04:14:51 +0200

    Create a SignatureController and add an endpoint for signing documents

---
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/controller/SignatureController.java
@@ -0,0 +1,23 @@
+package com.github.mostafaism1.etaeinvoicesigner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.github.mostafaism1.etaeinvoicesigner.service.DocumentSigningService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Controller("/")
+public class SignatureController {
+
+    private DocumentSigningService documentSigningService;
+
+    @PostMapping("")
+    @ResponseBody
+    public String sign(@RequestBody String jsonDocument) {
+        String canonicalizedDocument = documentSigningService.canonicalize(jsonDocument);
+        byte[] signature = documentSigningService.sign(canonicalizedDocument);
+        return documentSigningService.generateSignedDocument(canonicalizedDocument, signature);
+    }
+}
