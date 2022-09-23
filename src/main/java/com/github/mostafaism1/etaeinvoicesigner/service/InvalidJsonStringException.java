3d7df61e235cef898d2e5612c490867272724b03
Mostafa Hassanein <me.cssign@gmail.com>
2022-09-23 04:13:56 +0200

    Delete unused classes

--- src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/InvalidJsonStringException.java
+++
@@ -1,7 +0,0 @@
-package com.github.mostafaism1.etaeinvoicesigner.service;
-
-public class InvalidJsonStringException extends RuntimeException {
-    public InvalidJsonStringException(String invalidJsonString) {
-        super(invalidJsonString + " is not a valid json.");
-    }
-}
