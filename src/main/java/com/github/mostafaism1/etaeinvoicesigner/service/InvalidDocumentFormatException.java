f941598469f9c5e577e5a25621db7bef61621558
Mostafa Hassanein <me.cssign@gmail.com>
2022-09-23 04:13:29 +0200

    Create exception class InvalidDocumentFormatException

---
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/InvalidDocumentFormatException.java
@@ -0,0 +1,7 @@
+package com.github.mostafaism1.etaeinvoicesigner.service;
+
+public class InvalidDocumentFormatException extends RuntimeException {
+    public InvalidDocumentFormatException(String invalidDocument) {
+        super(invalidDocument + " is not a valid document.");
+    }
+}
