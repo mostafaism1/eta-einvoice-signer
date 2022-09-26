04ff47d9d4244a514a93f05e38b8f8566a08e8ad
Mostafa Hassanein <me.cssign@gmail.com>
2022-09-26 21:51:07 +0200

    Refactor AbstractDocumentSigningService to use an abstract factory and remove the factory methods

--- src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/AbstractDocumentSigningService.java
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/AbstractDocumentSigningService.java
@@ -2,25 +2,26 @@ package com.github.mostafaism1.etaeinvoicesigner.service;
 
 public abstract class AbstractDocumentSigningService implements DocumentSigningService {
 
+    private DocumentSigningFactory documentSigningFactory;
+
+    protected AbstractDocumentSigningService(DocumentSigningFactory documentSigningFactory) {
+        this.documentSigningFactory = documentSigningFactory;
+    }
+
     @Override
     public String canonicalize(String document) throws InvalidDocumentFormatException {
-        return createCanonicalizationStrategy().canonicalize(document);
+        return documentSigningFactory.getCanonicalizationStrategy().canonicalize(document);
     }
 
     @Override
     public byte[] sign(String data) {
-        return createSigningStrategy().sign(data);
+        return documentSigningFactory.getSigningStrategy().sign(data);
     }
 
     @Override
     public String generateSignedDocument(String document, byte[] signature) {
-        return createSignatureMergeStrategy().generateSignedDocument(document, signature);
+        return documentSigningFactory.getSignatureMergeStrategy()
+                .generateSignedDocument(document, signature);
     }
-
-    protected abstract CanonicalizationStrategy createCanonicalizationStrategy();
-
-    protected abstract SigningStrategy createSigningStrategy();
-
-    protected abstract SignatureMergeStrategy createSignatureMergeStrategy();
 
 }
