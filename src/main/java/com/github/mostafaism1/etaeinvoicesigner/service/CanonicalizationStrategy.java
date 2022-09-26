7803cfef933a7bd58eeeffcd343e5ed5197dc20b
Mostafa Hassanein <me.cssign@gmail.com>
2022-09-26 20:58:31 +0200

    Define a CanonicalizationStrategy interface

---
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/CanonicalizationStrategy.java
@@ -0,0 +1,14 @@
+package com.github.mostafaism1.etaeinvoicesigner.service;
+
+public interface CanonicalizationStrategy {
+
+    /**
+     * Transforms a document to its canonical format.
+     * 
+     * @param document a valid document
+     * @return the document in the canonical format
+     * @throws InvalidDocumentFormatException if the document's format is invalid
+     */
+    String canonicalize(String document) throws InvalidDocumentFormatException;
+
+}
