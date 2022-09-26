3f98d06c240cb201f6021760eeb1ac8db1e3ec18
Mostafa Hassanein <me.cssign@gmail.com>
2022-09-26 21:54:10 +0200

    Refactor ETADocumentSigningService by removing the factory methods and supplying a concrete factory to its parent

--- src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/ETADocumentSigningService.java
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/ETADocumentSigningService.java
@@ -5,19 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class ETADocumentSigningService extends AbstractDocumentSigningService {
 
-    @Override
-    protected CanonicalizationStrategy createCanonicalizationStrategy() {
-        return new ETAJsonCanonicalizationStrategy();
-    }
-
-    @Override
-    protected SigningStrategy createSigningStrategy() {
-        return new ETASigningStrategy();
-    }
-
-    @Override
-    protected SignatureMergeStrategy createSignatureMergeStrategy() {
-        return new ETASignatureMergeStrategy();
+    public ETADocumentSigningService(DocumentSigningFactory documentSigningFactory) {
+        super(new ETADocumentSigningFactory());
     }
 
 }
