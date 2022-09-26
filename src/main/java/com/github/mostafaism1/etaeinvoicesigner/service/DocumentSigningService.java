9687ff9ffd6bdee8d969db5f6c9d8f6741961cbe
Mostafa Hassanein <me.cssign@gmail.com>
2022-09-26 21:02:57 +0200

    Redefine the DocumentSigningService interface to extend other interfaces

--- src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/DocumentSigningService.java
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/DocumentSigningService.java
@@ -1,31 +1,6 @@
 package com.github.mostafaism1.etaeinvoicesigner.service;
 
-public interface DocumentSigningService {
-
-    /**
-     * Transforms a document to its canonical format.
-     * 
-     * @param document a valid document
-     * @return the document in the canonical format
-     * @throws InvalidDocumentFormatException if the document's format is invalid
-     */
-    String canonicalize(String document) throws InvalidDocumentFormatException;
-
-    /**
-     * Generates a cryptographic signature of the data.
-     * 
-     * @param data data to be signed
-     * @return the signature
-     */
-    byte[] sign(String data);
-
-    /**
-     * Merges a document and its signature into a single signed document.
-     * 
-     * @param document the original document
-     * @param signature the document's signature
-     * @return a signed document
-     */
-    String generateSignedDocument(String document, byte[] signature);
+public interface DocumentSigningService
+        extends CanonicalizationStrategy, SigningStrategy, SignatureMergeStrategy {
 
 }
