e9dec63e9d5a633da55691531c8e0d16c203a33a
Mostafa Hassanein <me.cssign@gmail.com>
2022-09-26 21:01:27 +0200

    Define a SignatureMergeStrategy interface

---
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/SignatureMergeStrategy.java
@@ -0,0 +1,14 @@
+package com.github.mostafaism1.etaeinvoicesigner.service;
+
+public interface SignatureMergeStrategy {
+
+    /**
+     * Merges a document and its signature into a single signed document.
+     * 
+     * @param document the original document
+     * @param signature the document's signature
+     * @return a signed document
+     */
+    String generateSignedDocument(String document, byte[] signature);
+
+}
