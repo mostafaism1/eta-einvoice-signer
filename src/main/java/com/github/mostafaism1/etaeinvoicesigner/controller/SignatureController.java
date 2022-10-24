aac4f67315985304c03a5942d6fb875296bd0a2b
Mostafa Hassanein <me.cssign@gmail.com>
2022-10-24 18:44:10 +0200

    Add an endpoint for bulk signing

--- src/main/java/com/github/mostafaism1/etaeinvoicesigner/controller/SignatureController.java
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/controller/SignatureController.java
@@ -1,21 +1,26 @@
 package com.github.mostafaism1.etaeinvoicesigner.controller;
 
+import com.github.mostafaism1.etaeinvoicesigner.service.DocumentSigningService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.github.mostafaism1.etaeinvoicesigner.service.DocumentSigningService;
-import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Controller("/")
 public class SignatureController {
+  private DocumentSigningService documentSigningService;
 
-    private DocumentSigningService documentSigningService;
+  @PostMapping("")
+  @ResponseBody
+  public String signDocument(@RequestBody String jsonDocument) {
+    return documentSigningService.generateSignedDocument(jsonDocument);
+  }
 
-    @PostMapping("")
-    @ResponseBody
-    public String sign(@RequestBody String jsonDocument) {
-        return documentSigningService.generateSignedDocument(jsonDocument);
-    }
+  @PostMapping("bulk")
+  @ResponseBody
+  public String signDocuments(@RequestBody String jsonDocuments) {
+    return documentSigningService.generateSignedDocuments(jsonDocuments);
+  }
 }
