e1626ce1c69c13e623747d65013ab3fff602aa3e
Mostafa Hassanein <me.cssign@gmail.com>
2022-10-24 19:06:13 +0200

    Remove @SpringBootTest annotation

--- src/test/java/com/github/mostafaism1/etaeinvoicesigner/service/ETADocumentSigningServiceTest.java
+++ src/test/java/com/github/mostafaism1/etaeinvoicesigner/service/ETADocumentSigningServiceTest.java
@@ -1,8 +1,5 @@
 package com.github.mostafaism1.etaeinvoicesigner.service;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 public class ETADocumentSigningServiceTest {
   private DocumentSigningService documentSigningService = new ETADocumentSigningService();
 }
