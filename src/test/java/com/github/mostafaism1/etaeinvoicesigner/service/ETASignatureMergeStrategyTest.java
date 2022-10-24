5eecf550f02dd6b5fae3aae0cf8d16a340d62c4b
Mostafa Hassanein <me.cssign@gmail.com>
2022-10-24 12:27:22 +0200

    Add a test case for ETASignatureMergeStrategy.merge

---
+++ src/test/java/com/github/mostafaism1/etaeinvoicesigner/service/ETASignatureMergeStrategyTest.java
@@ -0,0 +1,39 @@
+package com.github.mostafaism1.etaeinvoicesigner.service;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import org.junit.jupiter.api.Test;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+public class ETASignatureMergeStrategyTest {
+
+    private SignatureMergeStrategy signatureMergeStrategy = new ETASignatureMergeStrategy();
+
+    @Test
+    public void merged_document_should_have_correct_structure_and_value() {
+        // Given.
+        String document = "{key: \"value\"}";
+        String signatureValue = "signatureValue";
+        String signatures = """
+                [
+                      {
+                        "signatureType": "I",
+                        "value": "signatureValue"
+                      }
+                    ]""";
+        Gson gson = new Gson();
+        JsonObject documentAsJson = gson.fromJson(document, JsonObject.class);
+        JsonArray signaturesAsJson = gson.fromJson(signatures, JsonArray.class);
+        JsonObject expected = documentAsJson.deepCopy();
+        expected.add("signatures", signaturesAsJson);
+
+        // When.
+        String merged = signatureMergeStrategy.merge(document, signatureValue);
+        JsonObject actual = gson.fromJson(merged, JsonObject.class);
+
+        // Then.
+        then(actual).isEqualTo(expected);
+    }
+
+}
