8d1c3e6b4dbfcdbcdbc6618198f9bf872cf6bb86
Mostafa Hassanein <me.cssign@gmail.com>
2022-11-05 23:13:38 +0200

    Create and implement FileConfigurationReader

---
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/FileConfigurationReader.java
@@ -0,0 +1,50 @@
+package com.github.mostafaism1.etaeinvoicesigner.service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public enum FileConfigurationReader implements ConfigurationReader {
+  INSTANCE;
+
+  Properties properties;
+
+  private FileConfigurationReader() {
+    tryReadingConfiguration();
+  }
+
+  @Override
+  public String getPkcs11ConfigFilePath() {
+    return properties.getProperty("pkcs11ConfigFilePath");
+  }
+
+  @Override
+  public String getKeyStorePath() {
+    return properties.getProperty("keyStorePath");
+  }
+
+  @Override
+  public String getKeyStoreType() {
+    return properties.getProperty("keyStoreType");
+  }
+
+  @Override
+  public String getKeyStorePassword() {
+    return properties.getProperty("keyStorePassword");
+  }
+
+  @Override
+  public String getCertificateAlias() {
+    return properties.getProperty("certificateAlias");
+  }
+
+  private void tryReadingConfiguration() {
+    try {
+      properties = new Properties();
+      String configFilePath = System.getProperty("configFilePath");
+      properties.load(new FileInputStream(configFilePath));
+    } catch (IOException e) {
+      throw new RuntimeException();
+    }
+  }
+}
