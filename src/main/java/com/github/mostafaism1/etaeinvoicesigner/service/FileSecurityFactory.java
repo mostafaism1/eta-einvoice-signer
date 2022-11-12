95188f0591e4d189d08b304caa0ba62348f184e5
Mostafa Hassanein <me.cssign@gmail.com>
2022-11-12 13:39:59 +0200

    Perform an extract method refactoring on FileSecurityFactory

--- src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/FileSecurityFactory.java
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/FileSecurityFactory.java
@@ -1,12 +1,17 @@
 package com.github.mostafaism1.etaeinvoicesigner.service;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.Provider;
 import java.security.Security;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
@@ -15,11 +20,13 @@ public enum FileSecurityFactory implements SecurityFactory {
 
   private ConfigurationReader configurationReader;
   private Provider provider;
+  private KeyStore keyStore;
 
   private FileSecurityFactory() {
     configurationReader = FileConfigurationReader.INSTANCE;
     provider = new BouncyCastleProvider();
     addSecurityProvider();
+    initializeKeystore();
   }
 
   @Override
@@ -30,52 +37,53 @@ public enum FileSecurityFactory implements SecurityFactory {
   @Override
   public PrivateKey getPrivateKey() {
     try {
-      InputStream inputStream = Files.newInputStream(
-        Path.of(configurationReader.getKeyStorePath())
-      );
-      KeyStore keyStore = KeyStore.getInstance(
-        configurationReader.getKeyStoreType()
-      );
-      keyStore.load(
-        inputStream,
-        configurationReader.getKeyStorePassword().toCharArray()
-      );
       PrivateKey privateKey = (PrivateKey) keyStore.getKey(
         configurationReader.getCertificateAlias(),
         configurationReader.getKeyStorePassword().toCharArray()
       );
       return privateKey;
-    } catch (Exception e) {
-      System.out.println(e);
-      return null;
+    } catch (
+      UnrecoverableKeyException | KeyStoreException | NoSuchAlgorithmException e
+    ) {
+      throw new RuntimeException();
     }
   }
 
   @Override
   public X509Certificate getCertificate() {
     try {
-      InputStream inputStream = Files.newInputStream(
-        Path.of(configurationReader.getKeyStorePath())
-      );
-      KeyStore keyStore = KeyStore.getInstance(
-        configurationReader.getKeyStoreType()
-      );
-      keyStore.load(
-        inputStream,
-        configurationReader.getKeyStorePassword().toCharArray()
-      );
       X509Certificate x509Certificate = (X509Certificate) keyStore.getCertificate(
         configurationReader.getCertificateAlias()
       );
       return x509Certificate;
     } catch (Exception e) {
-      System.out.println(e);
-      return null;
+      throw new RuntimeException();
     }
   }
 
   @Override
   public Provider getProvider() {
     return provider;
+  }
+
+  private void initializeKeystore() {
+    try (
+      InputStream inputStream = Files.newInputStream(
+        Path.of(configurationReader.getKeyStorePath())
+      )
+    ) {
+      keyStore = KeyStore.getInstance(configurationReader.getKeyStoreType());
+      keyStore.load(
+        inputStream,
+        configurationReader.getKeyStorePassword().toCharArray()
+      );
+    } catch (
+      KeyStoreException
+      | NoSuchAlgorithmException
+      | CertificateException
+      | IOException e
+    ) {
+      throw new RuntimeException();
+    }
   }
 }
