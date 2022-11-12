8f99496eb691591b6317dd8cac659fa9d8bbc3df
Mostafa Hassanein <me.cssign@gmail.com>
2022-11-12 13:48:21 +0200

    Perform an extract method refactoring on HardwareTokenSecurityFactory

--- src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/HardwareTokenSecurityFactory.java
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/service/HardwareTokenSecurityFactory.java
@@ -1,9 +1,13 @@
 package com.github.mostafaism1.etaeinvoicesigner.service;
 
+import java.io.IOException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.Provider;
 import java.security.Security;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
 public enum HardwareTokenSecurityFactory implements SecurityFactory {
@@ -14,11 +18,13 @@ public enum HardwareTokenSecurityFactory implements SecurityFactory {
 
   private ConfigurationReader configurationReader;
   private Provider provider;
+  private KeyStore keyStore;
 
   private HardwareTokenSecurityFactory() {
     provider = Security.getProvider(PROVIDER_NAME);
     addSecurityProvider();
     configurationReader = FileConfigurationReader.INSTANCE;
+    initializeKeystore();
   }
 
   @Override
@@ -31,42 +37,47 @@ public enum HardwareTokenSecurityFactory implements SecurityFactory {
   @Override
   public PrivateKey getPrivateKey() {
     try {
-      KeyStore keyStore = KeyStore.getInstance(KEY_STORE_TYPE);
-      keyStore.load(
-        null,
-        configurationReader.getKeyStorePassword().toCharArray()
-      );
       PrivateKey privateKey = (PrivateKey) keyStore.getKey(
         configurationReader.getCertificateAlias(),
         null
       );
       return privateKey;
     } catch (Exception e) {
-      System.out.println(e);
-      return null;
+      throw new RuntimeException();
     }
   }
 
   @Override
   public X509Certificate getCertificate() {
     try {
-      KeyStore keyStore = KeyStore.getInstance(KEY_STORE_TYPE);
-      keyStore.load(
-        null,
-        configurationReader.getKeyStorePassword().toCharArray()
-      );
       X509Certificate x509Certificate = (X509Certificate) keyStore.getCertificate(
         configurationReader.getCertificateAlias()
       );
       return x509Certificate;
     } catch (Exception e) {
-      System.out.println(e);
-      return null;
+      throw new RuntimeException();
     }
   }
 
   @Override
   public Provider getProvider() {
     return provider;
+  }
+
+  private void initializeKeystore() {
+    try {
+      keyStore = KeyStore.getInstance(KEY_STORE_TYPE);
+      keyStore.load(
+        null,
+        configurationReader.getKeyStorePassword().toCharArray()
+      );
+    } catch (
+      KeyStoreException
+      | NoSuchAlgorithmException
+      | CertificateException
+      | IOException e
+    ) {
+      throw new RuntimeException();
+    }
   }
 }
