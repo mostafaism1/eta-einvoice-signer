94e01b800073d5b5d08f7df727cf7ab28076be19
Mostafa Hassanein <me.cssign@gmail.com>
2022-11-13 10:41:06 +0200

    Annotate SignatureController with @RestController

--- src/main/java/com/github/mostafaism1/etaeinvoicesigner/controller/SignatureController.java
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/controller/SignatureController.java
@@ -2,26 +2,23 @@ package com.github.mostafaism1.etaeinvoicesigner.controller;
 
 import com.github.mostafaism1.etaeinvoicesigner.service.DocumentSigningService;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/sign")
 public class SignatureController {
   private DocumentSigningService documentSigningService;
 
   @PostMapping("single")
-  @ResponseBody
   public String signDocument(@RequestBody String jsonDocument) {
     return documentSigningService.generateSignedDocument(jsonDocument);
   }
 
   @PostMapping("bulk")
-  @ResponseBody
   public String signDocuments(@RequestBody String jsonDocuments) {
     return documentSigningService.generateSignedDocuments(jsonDocuments);
   }
