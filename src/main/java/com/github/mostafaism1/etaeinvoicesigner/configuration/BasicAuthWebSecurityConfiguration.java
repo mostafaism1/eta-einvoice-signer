a55a50a5f67a63bd1e31160cc7a686e6d1cf97a4
Mostafa Hassanein <me.cssign@gmail.com>
2022-11-13 10:13:07 +0200

    Configure Basic authentication

--- src/main/java/com/github/mostafaism1/etaeinvoicesigner/configuration/BasicAuthWebSecurityConfiguration.java
+++ src/main/java/com/github/mostafaism1/etaeinvoicesigner/configuration/BasicAuthWebSecurityConfiguration.java
@@ -4,14 +4,29 @@ import com.github.mostafaism1.etaeinvoicesigner.service.ConfigurationReader;
 import com.github.mostafaism1.etaeinvoicesigner.service.FileConfigurationReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class BasicAuthWebSecurityConfiguration {
   ConfigurationReader configurationReader = FileConfigurationReader.INSTANCE;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf()
+      .disable()
+      .authorizeRequests()
+      .anyRequest()
+      .authenticated()
+      .and()
+      .httpBasic();
+    return http.build();
+  }
 
   @Bean
   public UserDetailsService users() {
