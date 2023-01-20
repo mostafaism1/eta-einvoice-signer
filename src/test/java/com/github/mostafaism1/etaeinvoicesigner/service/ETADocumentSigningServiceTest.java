312071012c289db1e8aa90aa793f3d0ab2aeddcb
Mostafa Hassanein <me.cssign@gmail.com>
2023-01-20 21:08:53 +0200

    Delete empty test class

--- src/test/java/com/github/mostafaism1/etaeinvoicesigner/service/ETADocumentSigningServiceTest.java
+++
@@ -1,5 +0,0 @@
-package com.github.mostafaism1.etaeinvoicesigner.service;
-
-public class ETADocumentSigningServiceTest {
-  private DocumentSigningService documentSigningService = new ETADocumentSigningService();
-}
